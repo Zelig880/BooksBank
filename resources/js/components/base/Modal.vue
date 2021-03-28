ce7243b93f51dba6274ea441edbca9af69a49071
Simone Cuomo <scuomo1988@gmail.com>
2021-03-28 00:34:54 +0000

    WIP borrow modal

--- resources/js/components/base/Modal.vue
+++ resources/js/components/base/Modal.vue
@@ -2,22 +2,10 @@
   <transition v-show="show" name="modal">
     <div class="modal-mask">
       <div class="modal-wrapper">
-        <div class="modal-container">
-          <div class="modal-header">
-            <slot name="header">
-              default header
-            </slot>
-          </div>
-
+        <div class="modal-container rounded-xl shadow-md mx-auto md:w-2/3 ">
           <div class="modal-body">
             <slot name="body">
               default body
-            </slot>
-          </div>
-
-          <div class="modal-footer">
-            <slot name="footer">
-              default footer
               <button class="modal-default-button" @click="$emit('close')">
                 OK
               </button>
@@ -60,27 +48,10 @@ export default {
 }
 
 .modal-container {
-  width: 300px;
-  margin: 0px auto;
-  padding: 20px 30px;
   background-color: #fff;
-  border-radius: 2px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
   transition: all 0.3s ease;
+  overflow:hidden;
   font-family: Helvetica, Arial, sans-serif;
-}
-
-.modal-header h3 {
-  margin-top: 0;
-  color: #42b983;
-}
-
-.modal-body {
-  margin: 20px 0;
-}
-
-.modal-default-button {
-  float: right;
 }
 
 /*
--- resources/js/components/sections/borrowModal.vue
+++ resources/js/components/sections/borrowModal.vue
@@ -1,7 +1,53 @@
 <template>
   <Modal :show="show">
     <template v-slot:body>
-      <date-picker :min-date="minDate" :max-date="maxDate" @dayclick="onDayClick" />
+      <div class="borrowModal grid grid-cols-6">
+        <div class="col-span-5 p-9 flex flex-wrap">
+          <div class="flex-grow">
+            <h2 class="text-2xl text-gray-700 font-bold" >Collection time available</h2>
+            <p class="text-gray-500 mb-12">Find the time that suit you best, from the available list</p>
+          </div>
+          <div class="w-4/6 pr-8">
+            <date-picker :is-expanded="true" title-position="left" :value="minDate" :min-date="minDate" :max-date="maxDate" @dayclick="onDayClick" />
+          </div>
+          <div class="w-2/6 pr-4">
+            <!-- sc88: make this dynamic -->
+            <p class="mb-3">Tuesday the xx of xxx</p>
+            <div class="time selected">8.00 - 10.00</div>
+            <div class="time">10.00 - 12.00</div>
+            <div class="time">12.00 - 14.00</div>
+            <div class="time">14.00 - 16.00</div>
+            <div class="time">16.00 - 18.00</div>
+            <div class="time">18.00 - 20.00</div>
+          </div>
+        </div>
+        <div class="col-span-1 bg-gray-300 p-9">
+          <h2>You are borrwing:</h2>
+          <!-- sc88: make this dynamic -->
+          <p>Book Title</p>
+          <!-- sc88: make this dynamic -->
+          <p>in CONDITIONTYPE condition</p>
+          <hr>
+          <!-- sc88: make this dynamic -->
+          <p>Borrower Username</p>
+          <!-- sc88: make this dynamic -->
+          <p>
+            <fa icon="map-marker-alt" />
+            2000 Miles
+          </p>
+          <hr>
+          <h2>Collection date:</h2>
+          <!-- sc88: make this dynamic -->
+          <p>Collection Date</p>
+          <h2>Collection time:</h2>
+          <!-- sc88: make this dynamic -->
+          <p>Collection Time</p>
+          <hr>
+          <h2>Book return date:</h2>
+          <!-- sc88: make this dynamic -->
+          <p>Bok return date</p>
+        </div>
+      </div>
     </template>
   </Modal>
 </template>
@@ -18,7 +64,7 @@ export default {
   },
   data () {
     return {
-      minDate: DateTime.now(),
+      minDate: DateTime.now().plus({ days: 1 }),
       maxDate: DateTime.now().plus({ weeks: 2 })
     }
   },
@@ -30,6 +76,19 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+.borrowModal{
+ hr{
+   @apply my-6 bg-gray-600;
+   height: 2px;
+ }
+  .time{
+    @apply rounded-lg border-2 border-gray-500 mt-2 w-auto px-7 py-4 text-center font-bold;
+
+    &.selected, &:hover{
+      @apply bg-gray-500 text-white cursor-pointer;
+    }
+  }
+}
 
 </style>
