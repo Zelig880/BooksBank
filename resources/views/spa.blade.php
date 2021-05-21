308da4f6420d15d50e08148069fb86c43662d6da
Abhishek Bodapati <abhishek.bodapati@gmail.com>
2021-05-21 11:36:52 +0530

    Added favicon

--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -12,6 +12,20 @@ $config = [
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+  <!-- Adding Favicon -->
+  <!-- Images in favicon folder -->
+	<link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+	<link rel="icon" type="image/png" sizes="192x192" href="favicon/android-chrome-192x192.png">
+	<link rel="icon" type="image/png" sizes="512x512" href="favicon/android-chrome-512x512.png">
+	<link rel="icon" href="favicon/favicon.ico">
+	<link rel="manifest" href="favicon/site.webmanifest">
+	<link rel="mask-icon" href="favicon/safari-pinned-tab.svg">
+	<meta name="msapplication-TileColor" content="#da532c">
+	<meta name="theme-color" content="#ffffff">
+	<meta name="msapplication-config" content="favicon/browserconfig.xml" />
+
   <title>{{ config('app.name') }}</title>
 
   <link rel="stylesheet" href="{{ mix('dist/css/app.css') }}">
