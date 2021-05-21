c566f427a2b6f76a49d85ef4ef538349dd30d4b9
Abhishek Bodapati <abhishek.bodapati@gmail.com>
2021-05-21 12:00:28 +0530

    Added favicon

--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -17,8 +17,6 @@ $config = [
 	<link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
 	<link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
 	<link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
-	<link rel="icon" type="image/png" sizes="192x192" href="favicon/android-chrome-192x192.png">
-	<link rel="icon" type="image/png" sizes="512x512" href="favicon/android-chrome-512x512.png">
 	<link rel="icon" href="favicon/favicon.ico">
 	<link rel="manifest" href="favicon/site.webmanifest">
 	<link rel="mask-icon" href="favicon/safari-pinned-tab.svg">
