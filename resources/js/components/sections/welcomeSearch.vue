c9aec13f944d08015f00e832b4e91cae02ffed4b
Simone Cuomo <scuomo1988@gmail.com>
2021-05-21 23:10:08 +0100

    complete all homepage design

--- resources/js/components/sections/welcomeSearch.vue
+++ resources/js/components/sections/welcomeSearch.vue
@@ -1,15 +1,18 @@
 <template>
-  <main class="bg-gray-200 py-10">
+  <main class="welcomeSearch py-10">
     <div class="container mx-auto h-auto">
-      <div class="mb-6 md:w-3/6">
-        <h2 class="text-5xl font-bold mb-4">
+      <div class="inline-block mx-3 pb-4 md:pb-16 md:w-3/6 align-text-bottom">
+        <h2 class="Light-backgroundH1---h6H1">
           {{ $t('welcomeSearch-title') }}
         </h2>
         <p>{{ $t('welcomeSearch-paragraph') }}</p>
       </div>
+      <div class="hidden md:inline-block md:w-2/6">
+        <img class="welcomeSearch_illustration" src="/assets/img/search-for-books.svg" alt="Illustration of lady reading a book">
+      </div>
       <form class="md:bg-white md:w-4/5 md:rounded-full md:pl-10 md:flex h-auto md:h-16 " @submit.prevent="handleForm" @keydown="form.onKeydown($event)">
         <div class="bg-white md:bg-none rounded-full md:rounded-none flex flex-col md:flex-grow py-2 px-10 md:px-0 md:mr-4 w-10/12 md:w-auto mx-auto">
-          <label>Search all books or a specific title</label>
+          <label>Books title or All</label>
           <input v-model="form.searchTitle" :class="{ 'is-invalid': form.errors.has('searchTitle') }" type="text" placeholder="ISBN or Name">
         </div>
         <div class="bg-white md:bg-none rounded-full md:rounded-none flex flex-col md:flex-grow md:pl-4 py-2 px-10 md:px-0 mr-x mt-6 md:mt-0 border-l-2 w-10/12 md:w-auto mx-auto">
@@ -82,5 +85,31 @@ export default {
 <style lang="scss">
 #search-button{
   background-color: var(--outline);
+}
+.welcomeSearch{
+  background-color: var(--light-3);
+  h2{
+    margin: 0 0 23px;
+  }
+  p{
+    margin: 23px 0 0 6px;
+    font-family: OpenSans;
+    font-size: 28px;
+    line-height: 1.5;
+    letter-spacing: -0.57px;
+    color: var(--sub-text);
+  }
+  &_illustration{
+    width: 300px;
+    margin: 0 auto;
+  }
+  form{
+    label{
+      font-family: OpenSans;
+      font-size: 18px;
+      font-weight: 600;
+      color: var(--body-dark);
+    }
+  }
 }
 </style>
