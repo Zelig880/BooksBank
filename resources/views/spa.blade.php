f67ccada65f7cf9f38db321639a56fccbdced244
Abhishek Bodapati <abhishek.bodapati@gmail.com>
2021-05-27 09:24:30 +0530

    replicated googleanalytics branch

---
+++ resources/views/spa.blade.php
@@ -0,0 +1,59 @@
+@php
+$config = [
+    'appName' => config('app.name'),
+    'locale' => $locale = app()->getLocale(),
+    'locales' => config('app.locales'),
+    'githubAuth' => config('services.github.client_id'),
+];
+$measurement_id = env('GA_MEASUREMENT_ID', 'None'); //Retrieving measurement_id from .env
+@endphp
+
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Adding Favicon -->
+  <!-- Images in favicon folder -->
+	<link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+	<link rel="icon" href="favicon/favicon.ico">
+	<link rel="manifest" href="favicon/site.webmanifest">
+	<link rel="mask-icon" href="favicon/safari-pinned-tab.svg">
+	<meta name="msapplication-TileColor" content="#da532c">
+	<meta name="theme-color" content="#ffffff">
+	<meta name="msapplication-config" content="favicon/browserconfig.xml" />
+  
+  <!-- Global site tag (gtag.js) - Google Analytics --> 
+  <!-- This block only runs of the measurement_id is provided -->
+  @if($measurement_id !== "None") 
+    <script>
+      var m_id = '{{ $measurement_id }}';
+    </script>
+    <script async src='https://www.googletagmanager.com/gtag/js?id=' + m_id></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', m_id);
+    </script>
+  @endif
+
+  <title>{{ config('app.name') }}</title>
+  <link rel="stylesheet" href="{{ mix('dist/css/app.css') }}">
+</head>
+<body>
+  <div id="app"></div>
+
+  {{-- Global configuration object --}}
+  <script>
+    window.config = @json($config);
+  </script>
+
+  {{-- Load the application scripts --}}
+  <script src="{{ mix('dist/js/app.js') }}"></script>
+</body>
+</html>
