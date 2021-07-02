cab500b30c7898d4a98d1f42a1e301ab9f761764
Simone Cuomo <scuomo1988@gmail.com>
2021-07-02 14:10:46 +0100

    Update About page look and feel of text

--- resources/js/pages/about.vue
+++ resources/js/pages/about.vue
@@ -11,15 +11,15 @@
 
     <h3>The development</h3>
     <p>The idea was pretty simple, the website development a bit less :)</p>
-    <p>Following the first idea Simone (<a href="www.zelig880.com" target="_blank">Zelig880</a>) started a simple market research between friends and family to get ideas on how to be develop this application</p>
+    <p>Following the first idea Simone (<a class="font-semibold underline" href="www.zelig880.com" target="_blank">Zelig880</a>) started a simple market research between friends and family to get ideas on how to be develop this application</p>
     <p>Following the initial idea, Simone has found some fantastic contributors that have given their time and energy in developing the application that you are using today</p>
     <h4>Special Thanks</h4>
-    <ul>
+    <ul class="list-disc pl-4">
       <li>My Wife for suffering my endless chat about the project</li>
       <li>Tircoed forest village community and Friends for helping me with the market research</li>
       <li>Robin Powell for the fantastic designs provided</li>
       <li>John for the amazing development contributions</li>
-      <li>All the other fantastic <a href="https://github.com/Zelig880/BooksBank/graphs/contributors" target="_blank">contributors</a> (at time of writing this page: abhishek-bodapati, sosualfred, priyalnym123, Ojsholly, venkivijay, markshenouda, marcusmyers)</li>
+      <li>All the other fantastic <a class="font-semibold underline" href="https://github.com/Zelig880/BooksBank/graphs/contributors" target="_blank">contributors</a> (at time of writing this page: abhishek-bodapati, sosualfred, priyalnym123, Ojsholly, venkivijay, markshenouda, marcusmyers)</li>
       <li>YOU.. Without your books this project would not exist</li>
     </ul>
   </div>
@@ -37,6 +37,9 @@ export default {
 #about {
   h3 {
     @apply text-2xl font-semibold mb-6;
+  }
+  h4{
+    @apply text-xl font-semibold mb-5;
   }
   p,
   ul {
