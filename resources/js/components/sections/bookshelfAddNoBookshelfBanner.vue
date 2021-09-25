3c6138fb7c8af725ffe6885c90be774913219d7b
This Dot <simone@thisdot.co>
2021-09-25 21:53:02 +0100

    Add the how to add a book video to help client

--- resources/js/components/sections/bookshelfAddNoBookshelfBanner.vue
+++ resources/js/components/sections/bookshelfAddNoBookshelfBanner.vue
@@ -2,6 +2,7 @@
   <div class="bookshelfAddNoBookshelfBanner flex flex-col rounded-md p-4 px-6">
     <h2 class="font-bold text-2xl mb-2">{{ $t('bookshelfAddNoBookshelfBanner-title') }}</h2>
     <p class="font-semibold" v-html="$t('bookshelfAddNoBookshelfBanner-paragraph')"></p>
+    <video controls src="/assets/video/how-to-add.mp4" />
     <Anchor theme="secondary" route-name="bookshelf.settings" class="mt-4">{{ $t('bookshelfAddNoBookshelfBanner-button') }}</Anchor>
   </div>
 </template>
