a31993bfe8e561d9debd5bcab8558ea61c89bc60
maxvanbel <maxvanbel@gmail.com>
2023-06-28 22:16:25 -0500

    make it better

--- resources/js/components/sections/bookshelfAddStep3.vue
+++ resources/js/components/sections/bookshelfAddStep3.vue
@@ -3,11 +3,11 @@
     <h3 class="text-2xl font-semibold mb-3"> {{ $t( 'bookshelfAdd-step3-title' ) }} </h3>
     <div class="grid grid-cols-1 md:grid-cols-2 gap-5 ml-8">
       <Button v-for="(Type, index) in transactionType" :key="Type.name" @click="selectType(index)" theme="square" :color="(selectedTypeIndex === index) ? 'primary' : ''">{{ Type.name }} </Button>
-      <div class="flex" v-if="selectedTypeIndex === 2">
+      <div class="flex items-center" v-if="selectedTypeIndex === 2">
         <label>
           {{ $t( 'bookshelfAdd-step3-price' ) }}
         </label>
-        <input type="number" step="0.01" :value="price" class="BookshelfAddBookshelf-field rounded-md border-2 pl-8 pr-6 py-2 block mb-4 mt-1" @keyup="$emit('update:price', $event.target.value)" @change="$emit('update:price', $event.target.value)">
+        <input type="number" step="0.01" value="0" class="BookshelfAddBookshelf-field rounded-md border-2 pl-8 pr-6 py-2 block mb-4 mt-1" @keyup="$emit('update:price', $event.target.value)" @change="$emit('update:price', $event.target.value)">
       </div>
     </div>
   </div>
